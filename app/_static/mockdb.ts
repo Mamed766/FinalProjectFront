27c8ca20e7899b0cafb1dc3b259bb234d5cf9391
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-12 14:28:54 +0400

    refactor:mockdb refactored

--- app/_static/mockdb.ts
+++ app/_static/mockdb.ts
@@ -1,8 +1,8 @@
 import { DropdownItem } from "../_components/Dropdown/Dropdown";
 
 export const homeItems: DropdownItem[] = [
-  { label: "HOME - BLAZERS STORE", link: "home-v1" },
-  { label: "HOME - WATCH STORE", link: "home-v2" },
-  { label: "HOME - JEWELLERY STORE", link: "home-v3" },
-  { label: "HOME - SPORTS WEAR", link: "home-v4" },
+  { label: "HOME - BLAZERS STORE", link: "/" },
+  { label: "HOME - WATCH STORE", link: "/" },
+  { label: "HOME - JEWELLERY STORE", link: "/" },
+  { label: "HOME - SPORTS WEAR", link: "/" },
 ];
