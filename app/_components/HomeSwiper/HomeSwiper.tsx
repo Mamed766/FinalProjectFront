03179924c43da4b1c110ec204d8a0b6b1f330ea5
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-12 17:44:24 +0400

    fixed:swiper

--- app/_components/HomeSwiper/HomeSwiper.tsx
+++ app/_components/HomeSwiper/HomeSwiper.tsx
@@ -4,13 +4,46 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
+import Image from "next/image";
 
 const HomeSwiper = () => {
   return (
     <div>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>
+          <div className="flex w-full h-full relative">
+            <div className="w-1/2">
+              <Image
+                alt="slider image 1"
+                src="https://darkfashion.wpengine.com/wp-content/uploads/2023/10/slider-1-a.webp"
+                height={1000}
+                width={1000}
+                layout="responsive"
+                quality={100}
+              />
+            </div>
+            <div className="w-1/2">
+              <Image
+                alt="slider image 2"
+                src="https://darkfashion.wpengine.com/wp-content/uploads/2023/10/slider-1-b.webp"
+                height={1000}
+                width={1000}
+                layout="responsive"
+                quality={100}
+              />
+            </div>
+
+            <div className="absolute left-[-6rem] top-[25rem]  text-white">
+              <ul className="flex gap-8  rotate-[270deg]">
+                <li className="hover:text-[#BB9D7B] duration-300">Facebook</li>
+                <li className="hover:text-[#BB9D7B] duration-300">Instagram</li>
+                <li className="hover:text-[#BB9D7B] duration-300">Twitter</li>
+                <li className="hover:text-[#BB9D7B] duration-300">Linkedin</li>
+              </ul>
+            </div>
+          </div>
+        </SwiperSlide>
+
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["darkfashion.wpengine.com"],
+  },
+};
 
 export default nextConfig;
