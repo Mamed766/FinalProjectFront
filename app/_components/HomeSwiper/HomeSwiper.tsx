dbef2a4ea5dceea1fca1e9ef0f065346797ece6c
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-12 18:17:47 +0400

    slide responsibility

--- app/_components/HomeSwiper/HomeSwiper.tsx
+++ app/_components/HomeSwiper/HomeSwiper.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -8,33 +7,31 @@ import Image from "next/image";
 
 const HomeSwiper = () => {
   return (
-    <div>
+    <div className="relative w-full">
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         <SwiperSlide>
-          <div className="flex w-full h-full relative">
-            <div className="w-1/2">
+          <div className="flex h-[60rem] relative">
+            <div className="w-1/2 relative h-full">
               <Image
                 alt="slider image 1"
                 src="https://darkfashion.wpengine.com/wp-content/uploads/2023/10/slider-1-a.webp"
-                height={1000}
-                width={1000}
-                layout="responsive"
+                layout="fill"
+                objectFit="cover"
                 quality={100}
               />
             </div>
-            <div className="w-1/2">
+            <div className="w-1/2 relative h-full">
               <Image
                 alt="slider image 2"
                 src="https://darkfashion.wpengine.com/wp-content/uploads/2023/10/slider-1-b.webp"
-                height={1000}
-                width={1000}
-                layout="responsive"
+                layout="fill"
+                objectFit="cover"
                 quality={100}
               />
             </div>
 
-            <div className="absolute left-[-6rem] top-[25rem]  text-white">
-              <ul className="flex gap-8  rotate-[270deg]">
+            <div className="absolute left-[-6rem] top-[25rem] text-white">
+              <ul className="flex gap-8 rotate-[270deg]">
                 <li className="hover:text-[#BB9D7B] duration-300">Facebook</li>
                 <li className="hover:text-[#BB9D7B] duration-300">Instagram</li>
                 <li className="hover:text-[#BB9D7B] duration-300">Twitter</li>
