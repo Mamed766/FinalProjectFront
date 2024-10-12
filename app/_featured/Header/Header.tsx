511bcd576da4f907b2584e3637c4cf4da61c2758
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-12 14:20:05 +0400

    chore:dropdown added

--- app/_featured/Header/Header.tsx
+++ app/_featured/Header/Header.tsx
@@ -1,17 +1,24 @@
 import AddToCartLogo from "@/app/_assets/AddToCartLogo";
 import OpenBarLogo from "@/app/_assets/OpenBarLogo";
 import SearchLogo from "@/app/_assets/SearchLogo";
+import Dropdown from "@/app/_components/Dropdown/Dropdown";
+import { homeItems } from "@/app/_static/mockdb";
 import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 const Header = () => {
   const pathname = usePathname();
+  const router = useRouter();
+
   const linkClass = (path: string) =>
     pathname === path
       ? `relative text-[#BB9D7B] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1px] after:bg-[#BB9D7B]`
       : `relative  hover:text-[#BB9D7B] duration-300  text-white after:content-[""] after:absolute   after:left-0 after:bottom-0 after:w-0 after:h-[1px] after:bg-[#BB9D7B] after:transition-all after:duration-300 hover:after:w-full`;
+
+  const handleNavigation = (path: string) => {
+    router.push(path);
+  };
 
   return (
     <div className="px-5 pb-5 pt-3 bg-black border-[1px] border-gray-600">
@@ -27,28 +34,64 @@ const Header = () => {
           />
         </div>
         <div>
-          <ul className="flex text-[14px] gap-7  mt-[15px]   ">
-            <Link href="/" className={linkClass("/")}>
-              HOME
-            </Link>
-            <Link href="/pages" className={linkClass("/pages")}>
-              PAGES
-            </Link>
-            <Link href="/gift-card" className={linkClass("/gift-card")}>
-              GIFT CARD
-            </Link>
-            <Link href="/gallery" className={linkClass("/gallery")}>
-              GALLERY
-            </Link>
-            <Link href="/shop" className={linkClass("/shop")}>
-              SHOP
-            </Link>
-            <Link href="/news" className={linkClass("/news")}>
-              NEWS
-            </Link>
-            <Link href="/contact" className={linkClass("/contact")}>
-              CONTACT
-            </Link>
+          <ul className="flex text-[14px] gap-7  mt-[15px]">
+            <li className={`${linkClass("/")} relative group`}>
+              <div
+                onClick={() => handleNavigation("/")}
+                className="cursor-pointer"
+              >
+                HOME
+                <Dropdown items={homeItems} />
+              </div>
+            </li>
+            <li className={linkClass("/pages")}>
+              <div
+                onClick={() => handleNavigation("/pages")}
+                className="cursor-pointer"
+              >
+                PAGES
+              </div>
+            </li>
+            <li className={linkClass("/gift-card")}>
+              <div
+                onClick={() => handleNavigation("/gift-card")}
+                className="cursor-pointer"
+              >
+                GIFT CARD
+              </div>
+            </li>
+            <li className={linkClass("/gallery")}>
+              <div
+                onClick={() => handleNavigation("/gallery")}
+                className="cursor-pointer"
+              >
+                GALLERY
+              </div>
+            </li>
+            <li className={linkClass("/shop")}>
+              <div
+                onClick={() => handleNavigation("/shop")}
+                className="cursor-pointer"
+              >
+                SHOP
+              </div>
+            </li>
+            <li className={linkClass("/news")}>
+              <div
+                onClick={() => handleNavigation("/news")}
+                className="cursor-pointer"
+              >
+                NEWS
+              </div>
+            </li>
+            <li className={linkClass("/contact")}>
+              <div
+                onClick={() => handleNavigation("/contact")}
+                className="cursor-pointer"
+              >
+                CONTACT
+              </div>
+            </li>
           </ul>
         </div>
         <div>
