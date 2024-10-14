fc6803b42c6a1abf0124aa15e421004ddbb7fc1c
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-14 17:19:47 +0400

    style:update profile

--- app/updateprofile/page.tsx
+++ app/updateprofile/page.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import { getCookie, setCookie } from "cookies-next";
+import Breadcrump from "../_components/breadcrump/Breadcrump";
+import Link from "next/link";
 
 const UpdateProfile = () => {
   const [formData, setFormData] = useState({
@@ -50,38 +52,81 @@ const UpdateProfile = () => {
   };
 
   return (
-    <div className="pt-20">
-      <form className="flex flex-col" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-          placeholder="First Name"
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          placeholder="Last Name"
-        />
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          placeholder="Username"
-        />
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder="Enter your current password"
-        />
-        <button type="submit">Update Profile</button>
-      </form>
+    <div className="pt-16">
+      <Breadcrump title="Update" bread1="Home" />
+      <div>
+        <div className="bg-black py-20 flex justify-center">
+          <div className="flex flex-col w-[35rem] py-20 px-10 bg-white">
+            <div className="text-center">
+              <h2 className="text-[#BB9D7B] text-[50px]">Update Profile</h2>
+            </div>
+            <form className="flex flex-col" onSubmit={handleSubmit}>
+              <div className="relative mb-4" data-twe-input-wrapper-init>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  placeholder="First Name"
+                  className="peer block min-h-[auto] border-b-[1px] w-full border-black bg-transparent py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary"
+                />
+              </div>
+
+              <div className="relative mb-4" data-twe-input-wrapper-init>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  placeholder="Last Name"
+                  className="peer block min-h-[auto] w-full border-black border-b-[1px] bg-transparent py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary"
+                />
+              </div>
+
+              <div className="relative mb-4" data-twe-input-wrapper-init>
+                <input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  placeholder="Username"
+                  className="peer block min-h-[auto] w-full border-black border-b-[1px] bg-transparent py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary"
+                />
+              </div>
+
+              <div className="relative mb-4" data-twe-input-wrapper-init>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Enter your current password"
+                  className="peer block min-h-[auto] w-full border-black border-b-[1px] bg-transparent py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary"
+                />
+              </div>
+
+              <div className="mb-12 pb-1 pt-1 text-center">
+                <button
+                  className="mb-3 inline-block bg-[#BB9D7B] w-full hover:bg-black px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-dark-3 transition duration-150 ease-in-out hover:shadow-dark-2 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:shadow-dark-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                  type="submit"
+                >
+                  Update Profile
+                </button>
+              </div>
+            </form>
+
+            <p className="flex gap-1 justify-center">
+              Return to
+              <Link
+                className="text-[#BB9D7B] cursor-pointer border-b-[1px] border-transparent hover:border-b-[1px] hover:border-[#BB9D7B] duration-700"
+                href={"/"}
+              >
+                Home?
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
