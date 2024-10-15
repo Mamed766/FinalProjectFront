c677bf24b4a6d8c8f03aeb5f8317de971f0e5724
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-15 22:25:13 +0400

    fix:loading style fixed

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex items-center z-50 justify-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-[#BB9D7B]"></div>
     </div>
   );
 }
