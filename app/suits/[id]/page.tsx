941437014cfaea3d0ef6deb7ddff65b3adbe0136
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-16 21:29:28 +0400

    fix:add-to card

--- app/suits/[id]/page.tsx
+++ app/suits/[id]/page.tsx
@@ -17,6 +17,8 @@ import {
   Box,
 } from "@chakra-ui/react";
 import InterestingStar from "@/app/_assets/InterestingStar";
+import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 
 const SuitProducts = () => {
   const [selectedSize, setSelectedSize] = useState<string | null>(null);
@@ -29,6 +31,8 @@ const SuitProducts = () => {
     minutes: 0,
     seconds: 0,
   });
+
+  const router = useRouter();
 
   const colors = [
     { name: "Pink", colorCode: "bg-pink-500" },
@@ -48,6 +52,15 @@ const SuitProducts = () => {
       module: "suitApi",
     }
   );
+
+  const {
+    data: SuitData,
+    isLoading: suitLoad,
+    error: isSuitError,
+  } = useRequest("suits", {
+    method: "GET",
+    module: "suitApi",
+  });
 
   useEffect(() => {
     if (data && data.suit && data.suit.stock) {
@@ -486,6 +499,82 @@ const SuitProducts = () => {
                     </AccordionPanel>
                   </AccordionItem>
                 </Accordion>
+              </div>
+            </div>
+          </div>
+          <div className="py-20 ">
+            <div className="">
+              <div className=" relative z-20">
+                <div className="flex flex-col gap-1 text-white justify-center items-center">
+                  <h2 className="text-[#BB9D7B]">TOP RATED</h2>
+                  <h1 className="text-[50px]">Related Product</h1>
+                </div>
+                <div className="flex  pt-5 gap-5 flex-wrap justify-center">
+                  {SuitData &&
+                    SuitData?.suits
+                      .slice(0, 3)
+                      .map((item: any, index: number) => {
+                        return (
+                          <div className="max-w-[30rem]  relative max-h-[48rem] group border border-transparent  duration-700">
+                            <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-[#BB9D7B] z-20 transition-all duration-700 group-hover:w-full"></span>
+                            <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-[#BB9D7B] z-20 transition-all duration-700 group-hover:h-full"></span>
+                            <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-[#BB9D7B] z-20 transition-all duration-700 group-hover:w-full"></span>
+                            <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-[#BB9D7B] z-20 transition-all duration-700 group-hover:h-full"></span>
+                            <div
+                              onClick={() => router.push(`${item._id}`)}
+                              className="relative max-h-[30rem] min-h-[30rem] h-full overflow-hidden cursor-pointer"
+                            >
+                              <motion.div
+                                initial={{ opacity: 1, scale: 1 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                whileHover={{ opacity: 0, scale: 0.8 }}
+                                transition={{ duration: 0.7 }}
+                              >
+                                <Image
+                                  alt=""
+                                  src={`http://localhost:3001/${item?.image1}`}
+                                  height={1000}
+                                  width={1000}
+                                  quality={100}
+                                  className="min-h-[30rem] max-h-[30rem] object-cover"
+                                />
+                              </motion.div>
+
+                              <motion.div
+                                className="absolute top-0 left-0"
+                                initial={{ opacity: 0, scale: 0.8 }}
+                                animate={{ opacity: 0, scale: 0.8 }}
+                                whileHover={{ opacity: 1, scale: 1 }}
+                                transition={{ duration: 0.7 }}
+                              >
+                                <Image
+                                  alt=""
+                                  src={`http://localhost:3001/${item?.image2}`}
+                                  height={1000}
+                                  width={1000}
+                                  quality={100}
+                                  className="min-h-[30rem] max-h-[30rem]  object-cover"
+                                />
+                              </motion.div>
+                            </div>
+                            <div className="text-center relative  overflow-hidden w-full  pt-10 pb-20">
+                              <h3 className="text-white text-[20px] cursor-pointer hover:text-[#BB9D7B] duration-700">
+                                {item?.title}
+                              </h3>
+                              <p className="text-gray-500">${item?.price}.00</p>
+                              <div className="w-full flex justify-center items-center">
+                                <button
+                                  onClick={() => router.push(`${item._id}`)}
+                                  className="text-white bg-[#BB9D7B] border border-transparent hover:border-[#BB9D7B] absolute bottom-[-1rem] opacity-0  group-hover:bottom-[1rem] group-hover:opacity-100 hover:bg-transparent duration-700  py-2 px-5 mt-2"
+                                >
+                                  Select Options
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      })}
+                </div>
               </div>
             </div>
           </div>
