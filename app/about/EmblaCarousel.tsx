36fee96d90724a133ae5b33fdffdb4f9e7aed68a
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-26 12:47:55 +0400

    chore:embla

---
+++ app/about/EmblaCarousel.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import { EmblaCarouselType, EmblaOptionsType } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from "./EmblaCarouselArrowButtons";
+
+type SlideType = { src: string; id: number };
+
+type PropType = {
+  slides: SlideType[];
+  options?: EmblaOptionsType;
+};
+
+const EmblaCarousel: React.FC<PropType> = ({ slides, options }) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  const onScroll = useCallback((emblaApi: EmblaCarouselType) => {
+    const progress = Math.max(0, Math.min(1, emblaApi.scrollProgress()));
+    setScrollProgress(progress * 100);
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+    onScroll(emblaApi);
+    emblaApi
+      .on("reInit", onScroll)
+      .on("scroll", onScroll)
+      .on("slideFocus", onScroll);
+  }, [emblaApi, onScroll]);
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map(({ src, id }) => (
+            <div className="embla__slide" key={id}>
+              <img
+                src={src}
+                alt={`Slide ${id}`}
+                className="embla__slide__img"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__progress">
+          <div
+            className="embla__progress__bar"
+            style={{ width: `${scrollProgress}%` }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmblaCarousel;
---
+++ app/about/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from "react";
+import Breadcrump from "../_components/breadcrump/Breadcrump";
+import Brand from "../_components/Brand/Brand";
+import Information from "../_components/Information/Information";
+import EmblaCarousel from "./EmblaCarousel";
+import { EmblaOptionsType } from "embla-carousel";
+import "./embla.css";
+
+const OPTIONS: EmblaOptionsType = { dragFree: true };
+const SLIDE_COUNT = 5;
+const SLIDES = [
+  {
+    src: "https://darkfashion.wpengine.com/wp-content/uploads/2023/09/team-image-2.webp",
+    id: 1,
+  },
+  {
+    src: "https://darkfashion.wpengine.com/wp-content/uploads/2023/09/team-image-3a.webp",
+    id: 2,
+  },
+  {
+    src: "https://darkfashion.wpengine.com/wp-content/uploads/2023/09/team-image-1.webp",
+    id: 3,
+  },
+  {
+    src: "https://darkfashion.wpengine.com/wp-content/uploads/2023/09/team-image-2.webp",
+    id: 4,
+  },
+];
+
+const About = () => {
+  const sliderStyle: React.CSSProperties = {
+    backgroundImage:
+      "url('https://darkfashion.wpengine.com/wp-content/uploads/2023/09/home-4-image-box-bg-image-a.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundAttachment: "fixed",
+    position: "relative",
+  };
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div>
+      <div className="pt-16">
+        <Breadcrump bread1="Home" title="About Us" />
+      </div>
+      <div>
+        <Brand />
+      </div>
+      <div>
+        <Information />
+      </div>
+      <div
+        style={sliderStyle}
+        className="py-20 flex justify-center items-center"
+      >
+        <div className="absolute top-0 bottom-0 left-0 right-0 bg-black opacity-80 z-10 w-full h-full"></div>
+
+        <div className="relative px-2 z-20 flex  flex-col md:flex-row justify-between gap-20 items-center">
+          <div className="text-white flex flex-col gap-1">
+            <div className="flex flex-col ">
+              <p className="text-[14px]">TAILORS & DESIGNERS</p>
+              <h2 className="text-[50px] max-w-[400px]">
+                Our varied fashion business is value-driven
+              </h2>
+              <p className="text-[16px] max-w-[500px]">
+                Sollicitudin ac orci phasellus egestas tellus. Amet nisl purus
+                in mollis nunc sed id. Cursus eget nunc scelerisque viverra
+                mauris in aliquam
+              </p>
+            </div>
+
+            <div className="mt-5">
+              <button className="bg-[#bb9d7b] hover:bg-black duration-700 px-10 py-2">
+                BOOK YOUR TRAINERS
+              </button>
+            </div>
+          </div>
+
+          <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
