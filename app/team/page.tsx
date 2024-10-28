c2deecce7ecddbe9d849e1f842ee5dd6d41c29d4
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-28 15:17:18 +0400

    chore:Services Home added to team

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Breadcrump from "../_components/breadcrump/Breadcrump";
-import Image from "next/image";
-import ShopLogo from "../_assets/ShopLogo";
-import { FaPlay } from "react-icons/fa";
-import { FaWindows } from "react-icons/fa";
-import { FaApple } from "react-icons/fa";
-import { IoLogoGooglePlaystore } from "react-icons/io5";
 import TeamCategory from "../_components/TeamCategory/TeamCategory";
 import TeamInfo from "../_components/TeamInfo/TeamInfo";
+import AOS from "aos";
+import ServicesHome from "../_components/ServicesHome/ServicesHome";
 
 const Team = () => {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
     <div>
       <div className="pt-16">
@@ -20,6 +22,9 @@ const Team = () => {
       </div>
       <div>
         <TeamInfo />
+      </div>
+      <div>
+        <ServicesHome />
       </div>
     </div>
   );
