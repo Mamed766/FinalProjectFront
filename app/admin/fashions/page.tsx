5b5a4e2569f39a50fe8e5719af711155c96a715d
Mamed766 <mehemmeddemirli57@gmail.com>
2024-11-03 13:56:47 +0400

    style:fashions updated

--- app/admin/fashions/page.tsx
+++ app/admin/fashions/page.tsx
@@ -64,7 +64,7 @@ const Page = () => {
           handleSideBar={handleSideBar}
         />
       </div>
-      <div className="px-10 mt-[5rem] py-20">
+      <div className="px-10 mt-[3rem] py-20">
         <button
           onClick={handleAddNew}
           className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
